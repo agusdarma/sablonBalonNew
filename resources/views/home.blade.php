ae8885cc49e67837eec4c84639b77f961be23f09
Agus Darma Kusuma <agus.darma@emobile.com.sg>
2018-04-04 17:10:43 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,131 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="content-wrapper">
-  <section class="content-header">
-    <h1>
-      Page Header
-      <small>Optional description</small>
-    </h1>
-    <ol class="breadcrumb">
-      <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-      <li class="active">Here</li>
-    </ol>
-  </section>
+<div class="content">
+  <!--best-->
+  <div class="best-w3">
+    <div class="container">
+      <h2 class="tittle">Why choose us?? </h2>
+      <div class="best-grids">
+        <div class="col-md-13 best-grid1">
+          <img src="{{asset('roasting/images/picrame.jpg')}}" class="img-responsive gray" alt="contoh hasil sablon balon"/>
+        </div>
+        <div class="col-md-8 best-grid">
+          <h4>What we offer </h4>
+          <p>Harga yang bersaing dan ketersediaan stock yang terjamin di dukung  pula dengan ekspedisi yang terpercaya kita akan memberikan yang terbaik untuk konsumen</p>
+          <div class="best-bottom">
+            <div class="col-md-6 best-left">
+              <div class="icons">
+                <i class="glyphicon glyphicon-time" aria-hidden="true"></i>
+              </div>
+              <h5>On time delivery</h5>
+              <p>Kami menggunakan jasa pengiriman terpercaya sehingga sangat kecil kemungkinanannya untuk terlambat</p>
+            </div>
+            <div class="col-md-6 best-right">
+              <div class="icons">
+                <i class="glyphicon glyphicon-credit-card" aria-hidden="true"></i>
+              </div>
+              <h5>Reasonable Price</h5>
+              <p>Harga yang di tawarkan cukup bersaing karena kami produsen bukan reseller</p>
+            </div>
+            <div class="clearfix"></div>
+          </div>
+          <div class="best-bottom">
+            <div class="col-md-6 best-left">
+              <div class="icons">
+                <i class="glyphicon glyphicon-floppy-saved" aria-hidden="true"></i>
+              </div>
+              <h5>Stock availability</h5>
+              <p>Ketersediaan stock terjamin karena kami memproduksi produk sendiri</p>
+            </div>
+            <div class="col-md-6 best-right">
+              <div class="icons">
+                <i class="glyphicon glyphicon-certificate" aria-hidden="true"></i>
+              </div>
+              <h5>Best quality</h5>
+              <p>Untuk memenuhi kebutuhan pelanggan pemilihan bahan baku untuk produksi dan tinta sudah menjadi prioritas kami sehingga tidak usah di ragukan lagi</p>
+            </div>
+            <div class="clearfix"></div>
+          </div>
+        </div>
+        <div class="clearfix"></div>
+      </div>
+    </div>
+  </div>
+  <!--best-->
+  <!--menu-->
+  <div class="special-w3">
+    <div class="container">
+      <h3 class="tittle1">Special Design</h3>
+      <div class="special-grids">
+        <div class="col-md-4 menu-grid">
+          <div class="menu1">
+            <img src="{{asset('roasting/images/pic14.jpg')}}" class="img-responsive gray" alt="sablon balon giant ekspres"/>
+          </div>
+        </div>
+        <div class="col-md-4 menu-grid">
+          <div class="menu1">
+            <img src="{{asset('roasting/images/pic8.jpg')}}" class="img-responsive gray" alt="sablon balon popbox.asia"/>
+          </div>
+        </div>
+        <div class="col-md-4 menu-grid">
+          <div class="menu1">
+            <img src="{{asset('roasting/images/pic9.jpg')}}" class="img-responsive gray" alt="sablon balon pop box"/>
+          </div>
+        </div>
+        <div class="clearfix"></div>
+      </div>
+      <div class="special-grids">
+        <div class="col-md-4 menu-grid">
+          <div class="menu1">
+            <img src="{{asset('roasting/images/pic10.jpg')}}" class="img-responsive gray" alt="sablon balon hisana"/>
+          </div>
+        </div>
+        <div class="col-md-4 menu-grid">
+          <div class="menu1">
+            <img src="{{asset('roasting/images/pic11.jpg')}}" class="img-responsive gray" alt="sablon balon ultah dhyara"/>
+          </div>
+        </div>
+        <div class="col-md-4 menu-grid">
+          <div class="menu1">
+            <img src="{{asset('roasting/images/pic12.jpg')}}" class="img-responsive gray" alt="sablon balon RSIA UMMI"/>
+          </div>
+        </div>
+        <div class="clearfix"></div>
+      </div>
+    </div>
+  </div>
+  <!--content-->
+  <div class="content">
+    <!--mission-->
+    <div class="special-w3">
+      <div class="container">
+        <h3 class="tittle1">Price List</h3>
+        <div class="special-grids">
+          <div class="col-md-4 menu-grid">
+            <div class="menu1">
+              <img src="{{asset('roasting/images/harga_dove.jpg')}}" class="img-responsive gray" alt="harga paket sablon balon dove"/>
+            </div>
+          </div>
+          <div class="col-md-4 menu-grid">
+            <div class="menu1">
+              <img src="{{asset('roasting/images/harga_besar.jpg')}}" class="img-responsive gray" alt="harga paket sablon balon besar"/>
+            </div>
+          </div>
+          <div class="col-md-4 menu-grid">
+            <div class="menu1">
+              <img src="{{asset('roasting/images/harga_metalik.jpg')}}" class="img-responsive gray" alt="harga paket sablon balon metalik"/>
+              <!-- <div class="menu-icon hvr-sweep-to-top">
+                <i class="glyphicon glyphicon-filter" aria-hidden="true"></i>
+              </div> -->
+            </div>
+          </div>
+          <div class="clearfix"></div>
+        </div>
+      </div>
+    </div>
+    <!--mission-->
+  </div>
 
-  <section class="content container-fluid">
-
-
-
-  </section>
 </div>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,12 +3,12 @@
 <head>
 <title>SablonBalon.com - Perusahaan Sablon Balon No 1 Di Indonesia</title>
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<!---->
+
 <link rel="stylesheet" href="{{asset('roasting/css/bootstrap.css')}}">
 <link rel="stylesheet" href="{{asset('roasting/css/style.css')}}">
 <link rel="shortcut icon" href="{{asset('roasting/images/favicon.ico')}}" type="image/x-icon">
 <link rel="icon" href="{{asset('roasting/images/favicon.ico')}}" type="image/x-icon">
-<!--for-mobile-apps-->
+
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta property="og:title" content="SablonBalon.com - Perusahaan Sablon Balon No 1 Di Indonesia" />
@@ -29,12 +29,9 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
   ga('send', 'pageview');
 
 </script>
-<!--//for-mobile-apps-->
 <script src="{{asset('roasting/js/jquery.min.js')}}"></script>
-<!---->
 <link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 <link href='//fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,700,300,200' rel='stylesheet' type='text/css'>
-<!---->
 </head>
 <body>
 	<script src="{{asset('roasting/js/jquery.vide.min.js')}}"></script>
@@ -68,15 +65,15 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 								<li><a data-hover="Contact" href="#">Contact</a></li>
 							</ul>
 						</nav>
-								<!-- script-for-menu -->
+
 									<script>
 									   $( "span.menu" ).click(function() {
 										 $( "ul.nav1" ).slideToggle( 300, function() {
-										 // Animation complete.
+
 										  });
 										 });
 									</script>
-								<!-- /script-for-menu -->
+
 					</div>
 					<div class="clearfix"></div>
 				</div>
@@ -90,133 +87,7 @@ function hideURLbar(){ window.scrollTo(0,1); } </script>
 	</div>
 	<!--banner-->
 	<!--content-->
-	<div class="content">
-		<!--best-->
-		<div class="best-w3">
-			<div class="container">
-				<h2 class="tittle">Why choose us?? </h2>
-				<div class="best-grids">
-					<div class="col-md-13 best-grid1">
-						<img src="{{asset('roasting/images/picrame.jpg')}}" class="img-responsive gray" alt="contoh hasil sablon balon"/>
-					</div>
-					<div class="col-md-8 best-grid">
-						<h4>What we offer </h4>
-						<p>Harga yang bersaing dan ketersediaan stock yang terjamin di dukung  pula dengan ekspedisi yang terpercaya kita akan memberikan yang terbaik untuk konsumen</p>
-						<div class="best-bottom">
-							<div class="col-md-6 best-left">
-								<div class="icons">
-									<i class="glyphicon glyphicon-time" aria-hidden="true"></i>
-								</div>
-								<h5>On time delivery</h5>
-								<p>Kami menggunakan jasa pengiriman terpercaya sehingga sangat kecil kemungkinanannya untuk terlambat</p>
-							</div>
-							<div class="col-md-6 best-right">
-								<div class="icons">
-									<i class="glyphicon glyphicon-credit-card" aria-hidden="true"></i>
-								</div>
-								<h5>Reasonable Price</h5>
-								<p>Harga yang di tawarkan cukup bersaing karena kami produsen bukan reseller</p>
-							</div>
-							<div class="clearfix"></div>
-						</div>
-						<div class="best-bottom">
-							<div class="col-md-6 best-left">
-								<div class="icons">
-									<i class="glyphicon glyphicon-floppy-saved" aria-hidden="true"></i>
-								</div>
-								<h5>Stock availability</h5>
-								<p>Ketersediaan stock terjamin karena kami memproduksi produk sendiri</p>
-							</div>
-							<div class="col-md-6 best-right">
-								<div class="icons">
-									<i class="glyphicon glyphicon-certificate" aria-hidden="true"></i>
-								</div>
-								<h5>Best quality</h5>
-								<p>Untuk memenuhi kebutuhan pelanggan pemilihan bahan baku untuk produksi dan tinta sudah menjadi prioritas kami sehingga tidak usah di ragukan lagi</p>
-							</div>
-							<div class="clearfix"></div>
-						</div>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-			</div>
-		</div>
-		<!--best-->
-		<!--menu-->
-		<div class="special-w3">
-			<div class="container">
-				<h3 class="tittle1">Special Design</h3>
-				<div class="special-grids">
-					<div class="col-md-4 menu-grid">
-						<div class="menu1">
-							<img src="{{asset('roasting/images/pic14.jpg')}}" class="img-responsive gray" alt="sablon balon giant ekspres"/>
-						</div>
-					</div>
-					<div class="col-md-4 menu-grid">
-						<div class="menu1">
-							<img src="{{asset('roasting/images/pic8.jpg')}}" class="img-responsive gray" alt="sablon balon popbox.asia"/>
-						</div>
-					</div>
-					<div class="col-md-4 menu-grid">
-						<div class="menu1">
-							<img src="{{asset('roasting/images/pic9.jpg')}}" class="img-responsive gray" alt="sablon balon pop box"/>
-						</div>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-				<div class="special-grids">
-					<div class="col-md-4 menu-grid">
-						<div class="menu1">
-							<img src="{{asset('roasting/images/pic10.jpg')}}" class="img-responsive gray" alt="sablon balon hisana"/>
-						</div>
-					</div>
-					<div class="col-md-4 menu-grid">
-						<div class="menu1">
-							<img src="{{asset('roasting/images/pic11.jpg')}}" class="img-responsive gray" alt="sablon balon ultah dhyara"/>
-						</div>
-					</div>
-					<div class="col-md-4 menu-grid">
-						<div class="menu1">
-							<img src="{{asset('roasting/images/pic12.jpg')}}" class="img-responsive gray" alt="sablon balon RSIA UMMI"/>
-						</div>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-			</div>
-		</div>
-		<!--content-->
-		<div class="content">
-			<!--mission-->
-			<div class="special-w3">
-				<div class="container">
-					<h3 class="tittle1">Price List</h3>
-					<div class="special-grids">
-						<div class="col-md-4 menu-grid">
-							<div class="menu1">
-								<img src="{{asset('roasting/images/harga_dove.jpg')}}" class="img-responsive gray" alt="harga paket sablon balon dove"/>
-							</div>
-						</div>
-						<div class="col-md-4 menu-grid">
-							<div class="menu1">
-								<img src="{{asset('roasting/images/harga_besar.jpg')}}" class="img-responsive gray" alt="harga paket sablon balon besar"/>
-							</div>
-						</div>
-						<div class="col-md-4 menu-grid">
-							<div class="menu1">
-								<img src="{{asset('roasting/images/harga_metalik.jpg')}}" class="img-responsive gray" alt="harga paket sablon balon metalik"/>
-								<!-- <div class="menu-icon hvr-sweep-to-top">
-									<i class="glyphicon glyphicon-filter" aria-hidden="true"></i>
-								</div> -->
-							</div>
-						</div>
-						<div class="clearfix"></div>
-					</div>
-				</div>
-			</div>
-			<!--mission-->
-		</div>
-
-	</div>
+	  @yield('content')
 		<!--footer-->
 			<div class="footer-w3l">
 				<div class="container">
